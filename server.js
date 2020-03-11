66320d2c769020f9a018e89cf16a01caf50cd22c
Alex Miller <alexmillerprojects@gmail.com>
2020-03-11 15:56:00 -0700

    working added get and post functionality

--- server.js
+++ server.js
@@ -1,13 +1,26 @@
-const express = require('express');
+const express = require("express");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const usersRouter = require("./users/userRouter");
 
 const server = express();
 
-server.get('/', (req, res) => {
+server.use(express.json());
+
+server.use("/users", usersRouter);
+
+server.get("/", logger, (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
 //custom middleware
 
-function logger(req, res, next) {}
+function logger(req, res, next) {
+  const method = req.method;
+  const endpoint = req.originalUrl;
+
+  console.log(`${method} to ${endpoint} | @ ${Date()}`);
+  next();
+}
 
 module.exports = server;
--- users/userRouter.js
+++ users/userRouter.js
@@ -1,39 +1,76 @@
-const express = require('express');
+const express = require("express");
+
+const Users = require("./userDb");
 
 const router = express.Router();
 
-router.post('/', (req, res) => {
+router.post("/", (req, res) => {
+  Users.get()
+    .then(user => {
+      res.status(200).json(user);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ errorMessage: "Error retrieving users" });
+    });
+});
+
+router.post("/:id/posts", (req, res) => {
   // do your magic!
 });
 
-router.post('/:id/posts', (req, res) => {
+router.get("/", (req, res) => {
+  Users.get()
+    .then(user => {
+      res.status(200).json(user);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ errorMessage: "Error retrieving users" });
+    });
+});
+
+router.get("/:id", (req, res) => {
+  const { id } = req.params;
+  Users.getById(id)
+    .then(user => {
+      res.status(200).json(user);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ errorMessage: "Error retrieving user" });
+    });
+});
+
+router.get("/:id/posts", (req, res) => {
   // do your magic!
 });
 
-router.get('/', (req, res) => {
+router.delete("/:id", (req, res) => {
   // do your magic!
 });
 
-router.get('/:id', (req, res) => {
-  // do your magic!
-});
-
-router.get('/:id/posts', (req, res) => {
-  // do your magic!
-});
-
-router.delete('/:id', (req, res) => {
-  // do your magic!
-});
-
-router.put('/:id', (req, res) => {
+router.put("/:id", (req, res) => {
   // do your magic!
 });
 
 //custom middleware
 
 function validateUserId(req, res, next) {
-  // do your magic!
+  const { id } = req.params;
+  Users.getById(id)
+    .then(user => {
+      if (id) {
+        console.log(user);
+        next();
+      } else {
+        res.status(400).json({ errorMessage: "User not found" });
+      }
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ errorMessage: "Error finding credentials" });
+    });
 }
 
 function validateUser(req, res, next) {
